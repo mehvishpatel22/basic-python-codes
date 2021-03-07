4d403f4e676a11b90f220301f4dcd44ba2f8fcee
mehvishpatel22 <80208813+mehvishpatel22@users.noreply.github.com>
2021-03-07 20:43:21 +0530

    Create Userinputsumoftwonumbers.py

---
+++ Userinputsumoftwonumbers.py
@@ -0,0 +1,6 @@
+num1 = input('enter first number:   ')
+enter first number:     1
+num2 = input('enter second number: ')
+enter second number: 5
+sum = num1 +num2
+print('the sum of {0} and {1} is {2}'.format(num1, num2, sum))
